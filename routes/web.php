dfa3ab3bc591ddd387421dd64694852d08afbc38
ridwanhoque <ridwanhoque2@gmail.com>
2020-11-06 09:59:28 +0600

    route page changed

--- routes/web.php
+++ routes/web.php
@@ -15,67 +15,6 @@ use App\Http\Controllers\TestController;
 |
 */
 
-
-/** */
-
-class Human{
-    public function eat(){
-        return "Eating";
-    }
-
-    public function play(){
-        return "Playing";
-    }
-}
-
-app()->bind('human', function(){
-    return new Human;
-});
-
-
-
-
-class Bike{
-    public function start(){
-        return "Starting";
-    }
-}
-
-app()->bind('bike', function(){
-    return new Bike;
-});
-
-
-class MainFacade{
-    public static function __callStatic($name, $arguments)
-    {
-        return app()->make(self::getFacadeAccess())->$name();
-    }
-
-    protected static function getFacadeAccess(){
-
-    }
-}
-
-
-class HumanFacade extends MainFacade{
-    protected static function getFacadeAccess(){
-        return 'human';
-    }
-}
-
-// class BikeFacade extends MainFacade{
-//     protected static function getFacadeAccess(){
-//         return 'bike';
-//     }
-// }
-
-dd(HumanFacade::eat());
-
-
-
-/** */
-
 Route::get('/', function () {
     return view('welcome');
 });
