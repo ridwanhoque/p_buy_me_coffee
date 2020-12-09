e40ef751085196fca2d2a2dde737c7e7c9ac9ee0
ridwanhoque <ridwan.abdnews@gmail.com>
2020-12-09 14:08:03 +0600

    login page modified

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,56 +1,53 @@
 <x-guest-layout>
-    <x-auth-card>
+    <x-jet-authentication-card>
         <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
+            <x-jet-authentication-card-logo />
         </x-slot>
-
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
-
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
-
+ 
+        <x-jet-validation-errors class="mb-4" />
+  
+        @if (session('status'))
+            <div class="mb-4 font-medium text-sm text-green-600">
+                {{ session('status') }}
+            </div>
+        @endif
+  
         <form method="POST" action="{{ route('login') }}">
             @csrf
-
-            <!-- Email Address -->
+ 
             <div>
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" required autofocus />
+                <x-jet-label value="Email" />
+                <x-jet-input class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
             </div>
-
-            <!-- Password -->
+  
             <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
+                <x-jet-label value="Password" />
+                <x-jet-input class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
             </div>
-
-            <!-- Remember Me -->
+  
             <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <input id="remember_me" type="checkbox" class="form-checkbox" name="remember">
+                <label class="flex items-center">
+                    <input type="checkbox" class="form-checkbox" name="remember">
                     <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                 </label>
             </div>
-
+  
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
-
-                <x-button class="ml-3">
+  
+                <x-jet-button class="ml-4">
                     {{ __('Login') }}
-                </x-button>
+                </x-jet-button>
+            </div>
+            <div class="flex items-center justify-end mt-4">
+                <a href="{{ url('auth/google') }}">
+                    <img src="https://developers.google.com/identity/images/btn_google_signin_dark_normal_web.png" style="margin-left: 3em;">
+                </a>
             </div>
         </form>
-    </x-auth-card>
+    </x-jet-authentication-card>
 </x-guest-layout>
