6119ff089e4dbde8ffab2710c9dbd414694d6850
ridwanhoque <ridwan.abdnews@gmail.com>
2020-12-09 11:45:11 +0600

    social login view page modified

--- resources/views/social_login/index.blade.php
+++ resources/views/social_login/index.blade.php
@@ -142,29 +142,7 @@ input[type=submit]:hover {
           <a href="#" class="google btn">
             <i class="fa fa-google fa-fw"></i> Login with Google+
           </a>
-        </div>
-  
-        <div class="col">
-          <div class="hide-md-lg">
-            <p>Or sign in manually:</p>
-          </div>
-  
-          <input type="text" name="username" placeholder="Username" required>
-          <input type="password" name="password" placeholder="Password" required>
-          <input type="submit" value="Login">
-        </div>
-  
+        </div>  
       </div>
     </form>
-  </div>
-  
-  <div class="bottom-container">
-    <div class="row">
-      <div class="col">
-        <a href="#" style="color:white" class="btn">Sign up</a>
-      </div>
-      <div class="col">
-        <a href="#" style="color:white" class="btn">Forgot password?</a>
-      </div>
-    </div>
   </div>
